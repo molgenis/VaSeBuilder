199f977561afacddeeba5ae853e48aa882ecb1fc
MatthieuBeukers <matthieu.beukers@gmx.com>
2019-04-12 10:25:41 +0200

    Add more quotes to VaSeEval variables

--- VaSeEval/VaSeEval.sh
+++ VaSeEval/VaSeEval.sh
@@ -19,35 +19,35 @@ cp "${ngsdnaVcf} ${resultsVcf}"
 
 
 # Create the temporary directory that will be used to create the combined VCF file
-mkdir tmp
+mkdir "${workingDir}/tmp"
 
 # Create the file with the VaSe sample name that we will use to reheader the VCF
-echo "VaSe" > tmp/vaseSample.txt
+echo "VaSe" > "${workingDir}/tmp/vaseSample.txt"
 
 
 # Read the donorvcf file (file containing a used donor files per sample) and copy the vcf files to a temporary directory as we will modify those as well (reheader).
 dvFiles=( $(cut -f 2 ${donorVcfListFile}) )
 for i in "${dvFiles[@]}"
 do
-	bcftools reheader -s tmp/vaseSample.txt -o tmp/"${i} ${i}"
+	bcftools reheader -s "${workingDir}/tmp/vaseSample.txt" -o "${workingDir}tmp/${i} ${i}"
 done
 
 # Concatenate all vcf.gz files into a vcf and compress it using bgzip from htslib.
-vcf-concat tmp/*.vcf.gz | bgzip > vaseeval.vcf.gz
+vcf-concat "${workingDir}/tmp/*.vcf.gz" | bgzip > "${workingDir}/vaseeval.vcf.gz"
 
 # Remove the INFO data from the combined VCF to avoid problems with the comparison
-bcftools annotate -x INFO vaseeval.vcf.gz
+bcftools annotate -x INFO "${workingDir}/vaseeval.vcf.gz"
 
 
 # Filter the vcf files to make the comparison easier
-bcftools query -f '%CHROM\t%POS\n' vaseeval.vcf.gz > vaseeval_chrompos.txt
-vcftools --positions vaseeval_chrompos.txt --gzvcf "${resultsVcf}" --recode --out filtered_vaseresult
-mv filtered_vaseresult.recode.vcf filtered_vaseresult.vcf
-bgzip filtered_vaseresult.vcf
+bcftools query -f '%CHROM\t%POS\n' "${workingDir}/vaseeval.vcf.gz" > "${workingDir}/vaseeval_chrompos.txt"
+vcftools --positions "${workingDir}/vaseeval_chrompos.txt" --gzvcf "${resultsVcf}" --recode --out "${workingDir}/filtered_vaseresult"
+mv "${workingDir}/filtered_vaseresult.recode.vcf" "${workingDir}/filtered_vaseresult.vcf"
+bgzip "${workingDir}/filtered_vaseresult.vcf"
 
 
 # Finally compare the two VCF files with vcf-compare_2
-vcf-compare_2.0.sh -1 ${workingDir}/tmp/vaseeval.vcf.gz -2 "${workingDir}/filtered_vaseresult.vcf.gz" -o "${workingDir}"
+vcf-compare_2.0.sh -1 "${workingDir}/tmp/vaseeval.vcf.gz" -2 "${workingDir}/filtered_vaseresult.vcf.gz" -o "${workingDir}"
 
 
 # Clean up afterwards (remove tmp dir and files)
