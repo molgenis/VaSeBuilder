e3f8e159b442e0b55b0ab7836b53e835d654f4ab
medinatd <tylerdanmedina@gmail.com>
2019-05-16 15:40:13 +0200

    Made PEP8 changes to DonorBamRead.py
    Mostly broke long lines and added periods to comments.

--- DonorBamRead.py
+++ DonorBamRead.py
@@ -1,8 +1,10 @@
 import statistics
 
+
 class DonorBamRead:
-    # Saves the required BAM read data
-    def __init__(self, readId, readPn, readChrom, readStart, readLen, readSeq, readQuals, mapQual):
+    # Saves the required BAM read data.
+    def __init__(self, readId, readPn, readChrom, readStart,
+                 readLen, readSeq, readQuals, mapQual):
         self.bamReadId = readId
         self.bamReadPairNum = readPn
         self.bamReadChrom = readChrom
@@ -12,36 +14,35 @@ class DonorBamRead:
         self.bamReadQual = readQuals
         self.bamReadMapQual = mapQual
 
-
-
-    # ====================METHODS TO GET SAVED DATA FROM THE DONORBAMREAD====================
+    # ===METHODS TO GET SAVED DATA FROM THE DONORBAMREAD=======================
     # Returns the BAM read identifier.
     def getBamReadId(self):
         return self.bamReadId
 
-    # Returns the BAM read pair number (1 or 2)
+    # Returns the BAM read pair number (1 or 2).
     def getBamReadPairNumber(self):
         return self.bamReadPairNum
 
-    # Returns the BAM read chromosome
+    # Returns the BAM read chromosome.
     def getBamReadChrom(self):
         return self.bamReadChrom
 
-    # Returns the BAM read starting position on the reference sequence
+    # Returns the BAM read starting position on the reference sequence.
     def getBamReadRefPos(self):
         return self.bamReadRefPos
 
-    # Returns the BAM read length
+    # Returns the BAM read length.
     def getBamReadLength(self):
         return self.bamReadLength
 
-    # Returns the BAM read ending position on the reference (calculated as starting position + the length of the read)
+    # Returns the BAM read ending position on the reference (calculated
+    # as starting position + the length of the read).
     def getBamReadRefEnd(self):
         if(self.bamReadLength is not None):
             return (self.bamReadRefPos + self.bamReadLength)
         return -1
 
-    # Returns the BAM read sequence
+    # Returns the BAM read sequence.
     def getBamReadSequence(self):
         return self.bamReadSeq
 
@@ -56,43 +57,54 @@ class DonorBamRead:
             qscores.append(ord(qualSymbol)-33)
         return qscores
 
-    # Returns the maping quality of the BAM read
+    # Returns the maping quality of the BAM read.
     def getMappingQual(self):
         return self.bamReadMapQual
 
-
-
-    # ====================METHOD TO GET STATISTICS DATA FROM THE DONORBAMREAD====================
-    # Returns the average Q-Score
+    # ===METHOD TO GET STATISTICS DATA FROM THE DONORBAMREAD===================
+    # Returns the average Q-Score.
     def getAverageQscore(self):
         qscores = self.getBamReadQScores()
         return statistics.mean(qscores)
 
-    # Returns the median Q-Score
+    # Returns the median Q-Score.
     def getMedianQScore(self):
         qscores = self.getBamReadQScores()
         return statistics.median(qscores)
 
-
-
-    # ====================METHODS TO CHECK WHETHER THE BAM READ IS R1 OR R2====================
-    # Returns if the BAM read is the first (forward) read
+    # ===METHODS TO CHECK WHETHER THE BAM READ IS R1 OR R2=====================
+    # Returns if the BAM read is the first (forward) read.
     def isRead1(self):
         return self.bamReadPairNum == '1'
 
-    # Returns if the BAM read is the second (reverse) read
+    # Returns if the BAM read is the second (reverse) read.
     def isRead2(self):
         return self.bamReadPairNum == '2'
 
-
-
-    # ====================METHODS TO RETURN A STRING REPRESENTATION OF THE DONORBAMREAD OBJECT====================
-    # Returns a String representation
+    # ====METHODS TO RETURN A STRING REPRESENTATION OF THE DONORBAMREAD OBJECT=
+    # Returns a String representation.
     def toString(self):
-        return str(self.bamReadId) +"\t"+ str(self.bamReadPairNum) +"\t"+ str(self.bamReadChrom) +"\t"+ str(self.bamReadRefPos) +"\t"+ str(self.bamReadLength) +"\t"+ str(self.bamReadSeq) +"\t"+ str(self.bamReadQual) +"\t"+ str(self.bamReadMapQual)
+        return (str(self.bamReadId) + "\t"
+                + str(self.bamReadPairNum) + "\t"
+                + str(self.bamReadChrom) + "\t"
+                + str(self.bamReadRefPos) + "\t"
+                + str(self.bamReadLength) + "\t"
+                + str(self.bamReadSeq) + "\t"
+                + str(self.bamReadQual) + "\t"
+                + str(self.bamReadMapQual))
 
-    # Returns the BAM read as a fastq sequence
+    # Returns the BAM read as a fastq sequence.
     def getAsFastQSeq(self, addPairNum=False):
         if(addPairNum):
-            return ("@"+ str(self.bamReadId) +"/"+ str(self.bamReadPairNum) +"\n"+ str(self.bamReadSeq) +"\n+\n"+ str(self.bamReadQual) +"\n")
-        return ("@"+ str(self.bamReadId) +"\n"+ str(self.bamReadSeq) +"\n+\n"+ str(self.bamReadQual) +"\n")
+            return ("@" + str(self.bamReadId) + "/" + str(self.bamReadPairNum)
+                    + "\n"
+                    + str(self.bamReadSeq)
+                    + "\n+\n"
+                    + str(self.bamReadQual)
+                    + "\n")
+        return ("@" + str(self.bamReadId)
+                + "\n"
+                + str(self.bamReadSeq)
+                + "\n+\n"
+                + str(self.bamReadQual)
+                + "\n")
