9a078b0edc3a25d31c0fa9134e86a4be59113bdf
MatthieuBeukers <matthieu.beukers@gmx.com>
2019-06-06 15:30:16 +0200

    Update to VaSeUtils and VariantContextFile reading method

--- VaSeUtils/CompareAcceptorContexts.py
+++ VaSeUtils/CompareAcceptorContexts.py
@@ -2,8 +2,9 @@ import logging
 from OverlapContext import OverlapContext
 
 class CompareAcceptorContexts:
-    def __init__(self):
+    def __init__(self, vaseutilhelper):
         self.vaseutillogger = logging.getLogger("VaSeUtil_Logger")
+        self.vuh = vaseutilhelper
 
     # Performs the actual comparison process
     def main(self, accconfileloc1, accconfileloc2):
@@ -12,7 +13,7 @@ class CompareAcceptorContexts:
         self.compare_acceptor_context_files(acc_contexts1, acc_contexts2)
 
     # Reads an acceptorcontext.txt file
-    def read_acceptor_context_file(self, accfileloc):
+    def read_acceptor_context_file(self, accfileloc, samplefilter=None, contextfilter=None, chromfilter=None):
         acceptor_contexts = {}
         try:
             with open(accfileloc, "r") as accfile:
@@ -20,9 +21,16 @@ class CompareAcceptorContexts:
                 for fileline in accfile:
                     fileline = fileline.strip()
                     filelinedata = fileline.split("\t")
-                    acc_context = OverlapContext(filelinedata[0], filelinedata[1], filelinedata[2], filelinedata[3],
-                                                 filelinedata[4], filelinedata[5], filelinedata[7].split(";"))
-                    acceptor_contexts[filelinedata[0]] = acc_context
+
+                    # Check whether the acceptor context passes any set filters
+                    samplepass = self.vuh.passes_filter(filelinedata[1], samplefilter)
+                    contextpass = self.vuh.passes_filter(filelinedata[0], contextfilter)
+                    chrompass = self.vuh.passes_filter(filelinedata[2], chromfilter)
+
+                    if samplepass and contextpass and chrompass:
+                        acc_context = OverlapContext(filelinedata[0], filelinedata[1], filelinedata[2], filelinedata[3],
+                                                     filelinedata[4], filelinedata[5], filelinedata[7].split(";"))
+                        acceptor_contexts[filelinedata[0]] = acc_context
         except IOError:
             self.vaseutillogger.warning(f"Could not read acceptor context file: {accfileloc}")
         return acceptor_contexts
@@ -31,9 +39,11 @@ class CompareAcceptorContexts:
     def compare_acceptor_context_files(self, acc_contexts1, acc_contexts2):
         shared_contexts = list(set(acc_contexts1.keys()) & set(acc_contexts2.keys()))
         for contextid in shared_contexts:
-            print(f"{contextid}")
+            varcon_compare_results = acc_contexts1[contextid].compare(acc_contexts2[contextid])
+            if len(varcon_compare_results) > 0:
+                print(f"{contextid}: {self.get_differences(varcon_compare_results)}")
 
-        #Display info about the acceptor contexts only present in one of the two acceptor context files.
+        # Display info about the acceptor contexts only present in one of the two acceptor context files.
         self.unique_acceptor_context_info(acc_contexts1, acc_contexts2)
         self.unique_acceptor_context_info(acc_contexts2, acc_contexts1)
 
@@ -46,6 +56,11 @@ class CompareAcceptorContexts:
                 print(self.get_differences(diffs))
 
     # Displays the differences between two shared acceptor contexts
-    def get_differences(self, difffields):
-        return ""
+    def get_differences(self, diffields):
+        difmap = self.vuh.get_accdon_context_fields()
+        differences = ""
+        for diffield in diffields:
+            if diffield in difmap:
+                differences += f"{difmap[diffield]}: {diffields[diffield][0]}/{diffields[diffield][1]}"
+        return differences
 
