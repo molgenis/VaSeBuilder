9bd1251cb744fa9e7d99eb6966aa5fafb5aa43d9
medinatd <tylerdanmedina@gmail.com>
2019-06-11 11:17:29 +0200

    Fixed np.percentiles key arg value.

--- VaSeBuilder.py
+++ VaSeBuilder.py
@@ -517,12 +517,12 @@ class VaSeBuilder:
             filtered_pos = []
 
             for pos_list in [starts, stops]:
-                q1 = np.percentile(pos_list, .25)
-                q3 = np.percentile(pos_list, .75)
+                q1 = np.percentile(pos_list, 25)
+                q3 = np.percentile(pos_list, 75)
                 iq = q3 - q1
                 filtered_pos = filtered_pos + [
                         pos for pos in pos_list
-                        if ((q1 - 10*iq) <= pos <= (q3 + 10*iq))
+                        if ((q1 - 3*iq) <= pos <= (q3 + 3*iq))
                         ]
 
             num_filtered = len(contextreads) - (len(filtered_pos) / 2)
