d2b9fc1d37739edd39a5db9e2091bba402a3050e
MatthieuBeukers <matthieu.beukers@gmx.com>
2019-06-12 10:54:37 +0200

    Change variable and methods names in VaSeUtils to comply with PEP8

--- VaSeUtils/VarconStatsFile.py
+++ VaSeUtils/VarconStatsFile.py
@@ -1,187 +1,197 @@
 import logging
 from VarconStats import VarconStats
 
+
 class VarconStatsFile:
-    def __init__(self, statsFileLoc, varconFilter=None):
-        self.vaseUtilLogger = logging.getLogger("VaSeUtil_Logger")
-        self.varconStatsData = self.readVarconStatsFile(statsFileLoc)
-    
+    def __init__(self, statsfileloc, contextfilter=None):
+        self.vaseutillogger = logging.getLogger("VaSeUtil_Logger")
+        self.varcon_stats_data = self.read_varcon_stats_file(statsfileloc, contextfilter)
+
     # Reads the variant context statistics file.
-    def readVarconStatsFile(self, fileLoc):
-        varconStats = {}
+    def read_varcon_stats_file(self, fileloc, contextfilter=None):
+        varcon_stats = {}
         try:
-            with open(fileLoc, 'r') as varconStatsFile:
-                next(varconStatsFile)    # Skips the header line
-                for fileLine in varconStatsFile:
-                    fileLine = fileLine.strip()
-                    fileLineData = fileLine.split("\t")
-                    varconStats[fileLineData[0]] = VarconStats(fileLineData[0], fileLineData[1], fileLineData[2], fileLineData[3], fileLineData[4], fileLineData[5], fileLineData[6], fileLineData[7], fileLineData[8], fileLineData[9], fileLineData[10], fileLineData[11], fileLineData[12])
+            with open(fileloc, 'r') as varconstatsfile:
+                next(varconstatsfile)    # Skips the header line
+                for fileline in varconstatsfile:
+                    fileline = fileline.strip()
+                    filelinedata = fileline.split("\t")
+                    if self.passes_filter(filelinedata[0], contextfilter):
+                        varcon_stats[filelinedata[0]] = VarconStats(filelinedata[0], filelinedata[1], filelinedata[2],
+                                                                    filelinedata[3], filelinedata[4], filelinedata[5],
+                                                                    filelinedata[6], filelinedata[7], filelinedata[8],
+                                                                    filelinedata[9], filelinedata[10], filelinedata[11],
+                                                                    filelinedata[12])
         except IOError as ioe:
-            self.vaseUtilLogger.warning("Could not read varcon stats file " +str(ioe.filename))
-        return varconStats
-    
-    
+            self.vaseutillogger.warning(f"Could not read varconstats file {ioe.filename}")
+        return varcon_stats
+
     # Returns the entire variant context statistics data map
-    def getVarconStatsData(self):
-        return self.varconStatsData
-    
+    def get_varcon_stats_data(self):
+        return self.varcon_stats_data
+
     # Returns an entire VarconStats object containing all statistics for a single variant context id.
-    def getVarconStats(self, varconId):
-        if(varconId in self.varconStatsData):
+    def get_varcon_stats(self, contextid):
+        if contextid in self.varcon_stats_data:
             return 
         return None
-    
+
     # Returns the variant context identifier
-    def getVariantContextId(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].get_variant_context_id()
+    def get_variant_context_id(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_variant_context_id()
         return None
-    
+
     # Returns the average acceptor read length of the variant context
-    def getAverageAcceptorReadLength(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getAverageAcceptorReadLength()
+    def get_average_acceptor_read_length(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_average_acceptor_read_length()
         return None
-    
+
     # Returns the average donor read length of the variant context
-    def getAverageDonorReadLength(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getAverageDonorReadLength()
+    def get_average_donor_read_length(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_average_donor_read_length()
         return None
-    
+
     # Returns the median acceptor read length of the variant context
-    def getMedianAcceptorReadLength(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getMedianAcceptorReadLength()
+    def get_median_acceptor_read_length(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_median_acceptor_read_length()
         return None
-    
+
     # Returns the median donor read length of the variant context
-    def getMedianDonorReadLength(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getMedianDonorReadLength()
+    def get_median_donor_read_length(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_median_donor_read_length()
         return None
-    
+
     # Returns the average acceptor read quality of the variant context
-    def getAverageAcceptorReadQuality(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getAverageAcceptorReadQuality()
+    def get_average_acceptor_read_quality(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_average_acceptor_read_quality()
         return None
-    
+
     # Returns the average donor read quality of the variant context
-    def getAverageDonorReadQuality(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getAverageDonorReadQuality()
+    def get_average_donor_read_quality(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_average_donor_read_quality()
         return None
-    
+
     # Returns the median acceptor read quality of the variant context
-    def getMedianAcceptorReadQuality(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getMedianAcceptorReadQuality()
+    def get_median_acceptor_read_quality(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_median_acceptor_read_quality()
         return None
-    
+
     # Returns the median donor read quality of the variant context
-    def getMedianDonorReadQuality(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getMedianDonorReadQuality()
+    def get_median_donor_read_quality(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_median_donor_read_quality()
         return None
-    
+
     # Returns the average acceptor mapq values of the variant context
-    def getAverageAcceptorMapQ(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getAverageAcceptorMapQ()
+    def get_average_acceptor_mapq(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_average_acceptor_mapq()
         return None
-    
+
     # Returns the average donor mapq values of the variant context
-    def getAverageDonorMapQ(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getAverageDonorMapQ()
+    def get_average_donor_mapq(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_average_donor_mapq()
         return None
-    
+
     # Returns the median acceptor mapq of the variant context
-    def getMedianAcceptorMapQ(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getMedianAcceptorMapQ()
+    def get_median_acceptor_mapq(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_median_acceptor_mapq()
         return None
-    
+
     # Returns the median donor mapq of the variant context
-    def getMedianDonorMapQ(self, varconId):
-        if(varconId in self.varconStatsData):
-            return self.varconStatsData[varconId].getMedianDonorMapQ()
+    def get_median_donor_mapq(self, contextid):
+        if contextid in self.varcon_stats_data:
+            return self.varcon_stats_data[contextid].get_median_donor_mapq()
         return None
-    
-    
+
     # Returns a list of all variant contexts
-    def getVariantContextsIds(self):
-        return list(self.varconStatsData.keys())
+    def get_variant_contexts_ids(self):
+        return list(self.varcon_stats_data.keys())
     
     # Returns a list/hashmap of all average acceptor read lengths
-    def getAverageAcceptorReadLengths(self, asList=False):
-        if(asList):
-            return [x.getAverageAcceptorReadLength() for x in list(self.varconStatsData.values())]
-        return {k:v.getAverageAcceptorReadLength() for k,v in self.varconStatsData.items()}
-    
+    def get_average_acceptor_read_lengths(self, aslist=False):
+        if aslist:
+            return [x.get_average_acceptor_read_length() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_average_acceptor_read_length() for k, v in self.varcon_stats_data.items()}
+
     # Returns a list/hashmap of all average donor read lengths
-    def getAverageDonorReadLengths(self, asList=False):
-        if(asList):
-            return [x.getAverageDonorReadLength() for x in list(self.varconStatsData.values())]
-        return {k:v.getAverageDonorReadLength() for k,v in self.varconStatsData.items()}
-    
+    def get_average_donor_read_lengths(self, aslist=False):
+        if aslist:
+            return [x.get_average_donor_read_length() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_average_donor_read_length() for k, v in self.varcon_stats_data.items()}
+
     # Returns a list/hashmap of all median ascceptor read lengths
-    def getMedianAcceptorReadLengths(self, asList=False):
-        if(asList):
-            return [x.getMedianAcceptorReadLength() for x in list(self.varconStatsData.values())]
-        return {k:v.getMedianAcceptorReadLength() for k,v in self.varconStatsData.items()}
-    
+    def get_median_acceptor_read_lengths(self, aslist=False):
+        if aslist:
+            return [x.get_median_acceptor_read_length() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_median_acceptor_read_length() for k, v in self.varcon_stats_data.items()}
+
     # Returns a list/hashmap of all median donor read lengths
-    def getMedianDonorReadLengths(self, asList=False):
-        if(asList):
-            return [x.getMedianDonorReadLength() for x in list(self.varconStatsData.values())]
-        return {k:v.getMedianDonorReadLength() for k,v in self.varconStatsData.items()}
-    
+    def get_median_donor_read_lengths(self, aslist=False):
+        if aslist:
+            return [x.get_median_donor_read_length() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_median_donor_read_length() for k, v in self.varcon_stats_data.items()}
+
     # Returns a list/hashmap of all average acceptor read qualities
-    def getAverageAcceptorReadQualities(self, asList=False):
-        if(asList):
-            return [x.getAverageAcceptorReadQuality() for x in list(self.varconStatsData.values())]
-        return {k:v.getAverageAcceptorReadQuality() for k,v in self.varconStatsData.items()}
-    
+    def get_average_acceptor_read_qualities(self, aslist=False):
+        if aslist:
+            return [x.get_average_acceptor_read_quality() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_average_acceptor_read_quality() for k, v in self.varcon_stats_data.items()}
+
     # Returns a list/hashmap of all average donor read qualities
-    def getAverageDonorReadQualities(self, asList=False):
-        if(asList):
-            return [x.getAverageDonorReadQuality() for x in list(self.varconStatsData.values())]
-        return {k:v.getAverageDonorReadQuality() for k,v in self.varconStatsData.items()}
-    
+    def get_average_donor_read_qualities(self, aslist=False):
+        if aslist:
+            return [x.get_average_donor_read_quality() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_average_donor_read_quality() for k, v in self.varcon_stats_data.items()}
+
     # Returns a list/hashmap of all median acceptor read qualities
-    def getMedianAcceptorReadQualities(self, asList=False):
-        if(asList):
-            return [x.getMedianAcceptorReadQuality() for x in list(self.varconStatsData.values())]
-        return {k:v.getMedianAcceptorReadQuality() for k,v in self.varconStatsData.items()}
-    
+    def get_median_acceptor_read_qualities(self, aslist=False):
+        if aslist:
+            return [x.get_median_acceptor_read_quality() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_median_acceptor_read_quality() for k, v in self.varcon_stats_data.items()}
+
     # Returns a list/hashmap of all median donor read qualities
-    def getMedianDonorReadQualities(self, asList=False):
-        if(asList):
-            return [x.getMedianDonorReadQuality() for x in list(self.varconStatsData.values())]
-        return {k:v.getMedianDonorReadQuality() for k,v in self.varconStatsData.items()}
-    
+    def get_median_donor_read_qualities(self, aslist=False):
+        if aslist:
+            return [x.get_median_donor_read_quality() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_median_donor_read_quality() for k, v in self.varcon_stats_data.items()}
+
     # Returns a list/hashmap of all average acceptor read mapq values
-    def getAverageAcceptorMapQs(self, asList=False):
-        if(asList):
-            return [x.getAverageAcceptorMapQ() for x in list(self.varconStatsData.values())]
-        return {k:v.getAverageAcceptorMapQ() for k,v in self.varconStatsData.items()}
-    
+    def get_average_acceptor_mapqs(self, aslist=False):
+        if aslist:
+            return [x.get_average_acceptor_mapq() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_average_acceptor_mapq() for k, v in self.varcon_stats_data.items()}
+
     # Returns a list/hashmap of all average donor read mapq values
-    def getAverageDonorMapQs(self, asList=False):
-        if(asList):
-            return [x.getAverageDonorMapQ() for x in list(self.varconStatsData.values())]
-        return {k:v.getAverageDonorMapQ() for k,v in self.varconStatsData.items()}
-    
+    def get_average_donor_mapqs(self, aslist=False):
+        if aslist:
+            return [x.get_average_donor_mapq() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_average_donor_mapq() for k, v in self.varcon_stats_data.items()}
+
     # Returns a list/hashmap of all median acceptor read mapq values
-    def getMedianAcceptorMapQs(self, asList=False):
-        if(asList):
-            return [x.getMedianAcceptorMapQ() for x in list(self.varconStatsData.values())]
-        return {k:v.getMedianAcceptorMapQ() for k,v in self.varconStatsData.items()}
+    def get_median_acceptor_mapqs(self, aslist=False):
+        if aslist:
+            return [x.get_median_acceptor_mapq() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_median_acceptor_mapq() for k, v in self.varcon_stats_data.items()}
     
     # Returns a list/hashmap of all median donor read mapq values
-    def getMedianDonorMapQs(self, asList=False):
-        if(asList):
-            return [x.getMedianDonorMapQ() for x in list(self.varconStatsData.values())]
-        return {k:v.getMedianDonorMapQ() for k,v in self.varconStatsData.items()}
+    def get_median_donor_mapqs(self, aslist=False):
+        if aslist:
+            return [x.get_median_donor_mapq() for x in list(self.varcon_stats_data.values())]
+        return {k: v.get_median_donor_mapq() for k, v in self.varcon_stats_data.items()}
+
+    # Checks whether a value is within the filter
+    def passes_filter(self, valtocheck, filterlist):
+        if filterlist is not None:
+            return valtocheck in filterlist
+        return True
