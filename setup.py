e927d604825edde01a267fae22915f3f1ec36849
tdmedina <tylerdanmedina@gmail.com>
2020-06-19 13:42:05 +0200

    Version update.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="VSB_test-tmedina",
-    version="0.0.2",
+    version="0.0.3",
     author="T. Medina",
     author_email="tylerdanmedina@gmail.com",
     description="VSB description.",
