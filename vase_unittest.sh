433cde15a939a27f5316024b274d12f1dd885165
tdmedina <tylerdanmedina@gmail.com>
2020-06-19 15:42:08 +0200

    Fixed extension.

---
+++ vase_unittest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Wrapper for VaSeBuilder to make annoying imports easier :)
+
+# Obtain the absolute path
+VBDIR=$( cd -P "$( dirname "$0" )" && pwd )
+
+# Add the diretory containing VaSeBuilder to the Python path so VaSeBuilder python files can easily be imported
+export PYTHONPATH=${PYTHONPATH}:${VBDIR}
+
+# Execute the VaSeBuilder program with all other provided parameters
+python ${VBDIR}/tests/vase_unittest.py
+
